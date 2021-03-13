74bbb96d8c443fd46987498b9dabd72cdeb64e0e
Anatoly V <ntnosteam@gmail.com>
2021-03-13 19:56:02 +0300

    format according to pep8

--- health_checks.py
+++ health_checks.py
@@ -6,9 +6,11 @@ import shutil
 import socket
 import psutil
 
+
 def check_reboot():
     """Return True if computer has a pending reboot"""
     return os.path.exists("run/reboot-required")
+
 
 def check_disk_full(disk, min_gb, min_percent):
     """Return True if there isn't enough disk space, False otherwise
@@ -20,31 +22,35 @@ def check_disk_full(disk, min_gb, min_percent):
     """
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
-    percent_free = 100 *du.free / du.total
+    percent_free = 100 * du.free / du.total
     # Calculate how maune free gigabytes
-    gigabytes_free = du.free / 2 **30
+    gigabytes_free = du.free / 2 ** 30
     if percent_free < min_percent or gigabytes_free < min_gb:
         return True
     return False
+
 
 def check_root_full():
     """Returns True if the root partitionis full, False otherwise"""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+
 def check_no_network():
     """Returns True if it fails to resolve Google's URL, False otherwise"""
-    try: 
+    try:
         socket.gethostbyname("www.google.com")
         return False
     except:
         return True
 
+
 def check_cpu_constrained():
     """Returns True if cpu is having too much usage, False otherwise."""
     return psutil.cpu_percent(1) > 75
 
+
 def main():
-    checks=[
+    checks = [
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full"),
         (check_no_network, "No working network."),
@@ -60,4 +66,6 @@ def main():
 
     print("Everything is ok.")
     sys.exit(0)
+
+
 main()
